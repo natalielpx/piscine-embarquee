3ad36e8ce44ee132a8b10c2b0cc5874f695ebff1
nlam <nlam@f1br6s5.paris.42.school>
2026-04-15 18:12:11 +0200

    [00-02] ft: add embd.h recording MACROS
    includes:
    required libraries
    constant definitions
    frequently used logic operations

--- md00/ex02/main.c
+++ md00/ex02/main.c
@@ -1,43 +1,27 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include "embd.h"
 
-#define LED_D1 PB0
-#define SWITCH_1 PD2
-#define INPUT 0
-#define OUTPUT 1
-#define LOW 0
-#define HIGH 1
+void setup( void ) {
 
-void set_pin( volatile uint8_t * reg, unsigned int pin, int state) {
-
-	if (state)
-		*reg |= (1 << pin);
-	else
-		*reg &= ~(1 << pin);
-}
-
-int read_pin( volatile uint8_t * reg, unsigned int pin) {
-
-	return !!(*reg & (1 << pin));
+	SET_PIN(DDRB, LED_D1, OUTPUT);		// Set PB0 to output
+	SET_PIN(DDRD, SWITCH_1, INPUT);		// Set PD2 to input
+	SET_PIN(PORTD, SWITCH_1, PULL_UP);	// Enable pull-up on PD2
 }
 
 int main( void ) {
 	
-	set_pin(&DDRB, LED_D1, OUTPUT);		// Set PB0 to output
-	set_pin(&DDRD, SWITCH_1, INPUT);	// Set PD2 to input
-	set_pin(&PORTD, SWITCH_1, 1);		// Enable pull-up on PD2
-
-	int prev = 0, curr = 0;
+	int prev = PULL_UP, curr = PULL_UP;
 
 	while (42) {
 
-		curr = read_pin(&PIND, SWITCH_1);
+		// Obtain current status of switch
+		curr = READ_PIN(PIND, SWITCH_1);
 
 		if (curr != prev) {
+			// Debounce
 			_delay_ms(50);
-			// if curr == 0, set LED_D1 to 1 (HIGH)
-			// if curr == 1, set LED_D1 to 0 (LOW)
-			set_pin(&PORTB, LED_D1, !curr);
+			// Turn LED_D1 on or off
+			SET_PIN(PORTB, LED_D1, !curr);
+			// Record status of switch
 			prev = curr;
 		}
 	}
