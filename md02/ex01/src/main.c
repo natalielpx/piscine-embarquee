a035b46640e8b0692f710296ffa84348f328bbd4
nlam <nlam@f1br3s5.paris.42.school>
2026-04-21 20:00:39 +0200

    [02-01] print string each second via Timer1
    interrupt vectors are used
    _delay_ms is not used

---
+++ md02/ex01/src/main.c
@@ -0,0 +1,31 @@
+#include "../embd.h"
+
+void setup( void ) {
+
+	// Components
+	uart_init();
+
+	// Timers
+	TCCR1B |= (1 << WGM12);		// Clear Timer on Compare match (CTC)
+	OCR1A = 62499;				// F_CPU / prescaler
+
+	// Interrupts
+	SREG = (1 << 7);				// Enable interrupts globally
+	TIMSK1 |= (1 << OCIE1A); 		// Enable interrupts specifically on Timer1
+	
+	// Start
+	TCCR1B |= (1 << CS12);		// prescaler /256
+}
+
+void TIMER1_COMPA_vect( void ) {
+
+	uart_printstr("Hello World!\r\n");
+}
+
+int main( void ) {
+
+	setup();
+
+    // Empty loop
+	while (42);
+}
