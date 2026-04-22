7d3862b7c7fb8a551aaaa99fa463d2b0dce23274
nlam <nlam@f1br10s3.paris.42.school>
2026-04-22 13:33:32 +0200

    [02-01] fix: correct print frequency
    frequency changed to every 2 seconds (as required by subject)

--- md02/ex01/src/main.c
+++ md02/ex01/src/main.c
@@ -7,14 +7,14 @@ void setup( void ) {
 
 	// Timers
 	TCCR1B |= (1 << WGM12);		// Clear Timer on Compare match (CTC)
-	OCR1A = 62499;				// F_CPU / prescaler
+	OCR1A = 31249;				// F_CPU / 1024 * 2 - 1
 
 	// Interrupts
 	SREG = (1 << 7);				// Enable interrupts globally
 	TIMSK1 |= (1 << OCIE1A); 		// Enable interrupts specifically on Timer1
 	
 	// Start
-	TCCR1B |= (1 << CS12);		// prescaler /256
+	TCCR1B |= (1 << CS12)| (1 << CS10);		// prescaler /1024
 }
 
 void __attribute__(( signal, used, externally_visible )) TIMER1_COMPA_vect( void ) {
